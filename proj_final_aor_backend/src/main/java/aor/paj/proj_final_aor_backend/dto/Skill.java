60b7ce610a357d93bdcd786fe0f2bc2ae6a39f34
Vânia Mendes <vaniamendes@student.dei.uc.pt>
2024-05-14 17:10:33 +0100

    dtos e entities criados

---
+++ proj_final_aor_backend/src/main/java/aor/paj/proj_final_aor_backend/dto/Skill.java
@@ -0,0 +1,41 @@
+package aor.paj.proj_final_aor_backend.dto;
+
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+/**
+ * This class represents a skill, which extends an interest.
+ */
+
+@XmlRootElement
+public class Skill extends Interest{
+
+    /**
+     * The type of the skill.
+     */
+    @XmlElement
+    private String type;
+
+
+    /**
+     * Default constructor.
+     */
+    public Skill() {
+    }
+
+    /**
+     * Returns the type of the skill.
+     * @return the type of the skill.
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Sets the type of the skill.
+     * @param type the type of the skill.
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+}
--- proj_final_aor_backend/src/main/java/aor/paj/proj_final_aor_backend/dto/User.java
+++ proj_final_aor_backend/src/main/java/aor/paj/proj_final_aor_backend/dto/User.java
@@ -1,4 +1,160 @@
 package aor.paj.proj_final_aor_backend.dto;
 
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+import java.time.LocalDateTime;
+
+/**
+ * This class represents a user.
+ */
+@XmlRootElement
 public class User {
+
+    @XmlElement
+    private long id;
+    @XmlElement
+    private String nickname;
+
+    @XmlElement
+    private String password;
+    @XmlElement
+    private String email;
+    @XmlElement
+    private String firstName;
+    @XmlElement
+    private String lastName;
+
+    @XmlElement
+    private boolean visibilityState;
+    @XmlElement
+    private boolean authState;
+    @XmlElement
+    private boolean activeState;
+    @XmlElement
+    private String resetPassToken;
+
+    @XmlElement
+    private String resetPassTokenExpiricy;
+    @XmlElement
+    private String biography;
+    @XmlElement
+    private LocalDateTime registerDate;
+    @XmlElement
+    private UserType userType;
+
+
+public User(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isVisibilityState() {
+        return visibilityState;
+    }
+
+    public void setVisibilityState(boolean visibilityState) {
+        this.visibilityState = visibilityState;
+    }
+
+    public boolean isAuthState() {
+        return authState;
+    }
+
+    public void setAuthState(boolean authState) {
+        this.authState = authState;
+    }
+
+    public boolean isActiveState() {
+        return activeState;
+    }
+
+    public void setActiveState(boolean activeState) {
+        this.activeState = activeState;
+    }
+
+    public String getResetPassToken() {
+        return resetPassToken;
+    }
+
+    public void setResetPassToken(String resetPassToken) {
+        this.resetPassToken = resetPassToken;
+    }
+
+    public String getResetPassTokenExpiricy() {
+        return resetPassTokenExpiricy;
+    }
+
+    public void setResetPassTokenExpiricy(String resetPassTokenExpiricy) {
+        this.resetPassTokenExpiricy = resetPassTokenExpiricy;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public LocalDateTime getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(LocalDateTime registerDate) {
+        this.registerDate = registerDate;
+    }
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
 }
