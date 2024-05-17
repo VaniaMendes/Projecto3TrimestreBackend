d8a9d1133ff00af98687d49ec9fe9767c5470274
Vânia Mendes <vaniamendes@student.dei.uc.pt>
2024-05-17 12:40:16 +0100

    Auto stash before merge of "vania" and "origin/vania"

--- proj_final_aor_backend/src/main/java/aor/paj/proj_final_aor_backend/dto/UserRegistration.java
+++ proj_final_aor_backend/src/main/java/aor/paj/proj_final_aor_backend/dto/UserRegistration.java
@@ -1,2 +1,42 @@
-package aor.paj.proj_final_aor_backend.dto;public class UserRegistration {
+package aor.paj.proj_final_aor_backend.dto;
+
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class UserRegistration {
+
+    @XmlElement
+    private String email;
+    @XmlElement
+    private String password;
+    @XmlElement
+    private String confirmPassword;
+
+    public UserRegistration() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
 }
