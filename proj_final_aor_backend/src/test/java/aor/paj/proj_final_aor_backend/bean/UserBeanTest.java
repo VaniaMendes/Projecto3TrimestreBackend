50c7ae34e63587df6bf9d6983064a5bdcd101234
Vânia Mendes <vaniamendes@student.dei.uc.pt>
2024-05-17 15:34:51 +0100

    testes registo

---
+++ proj_final_aor_backend/src/test/java/aor/paj/proj_final_aor_backend/bean/UserBeanTest.java
@@ -0,0 +1,76 @@
+package aor.paj.proj_final_aor_backend.bean;
+
+import aor.paj.proj_final_aor_backend.dao.UserDao;
+import aor.paj.proj_final_aor_backend.entity.UserEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+public class UserBeanTest {
+
+    @InjectMocks
+    UserBean userBean;
+
+    @Mock
+    UserDao userDao;
+
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void registerUser_withValidInput_returnsTrue() {
+        when(userDao.findUserByEmail(anyString())).thenReturn(null);
+
+        boolean result = userBean.registerUser("test@test.com", "Password1!", "Password1!");
+
+        assertTrue(result);
+        verify(userDao, times(1)).createUser(any(UserEntity.class));
+    }
+
+    @Test
+    public void registerUser_withExistingEmail_returnsFalse() {
+        when(userDao.findUserByEmail(anyString())).thenReturn(new UserEntity());
+
+        //User with the same email already exists
+        boolean result = userBean.registerUser("test@test.com", "Password1!", "Password1!");
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void registerUser_withInvalidPassword_returnsFalse() {
+        when(userDao.findUserByEmail(anyString())).thenReturn(null);
+
+        boolean result = userBean.registerUser("test1@test.com", "password", "password");
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void registerUser_withInvalidEmail_returnsFalse() {
+        boolean result = userBean.registerUser("test", "Password1!", "Password1!");
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void registerUser_withNonMatchingPasswords_returnsFalse() {
+        when(userDao.findUserByEmail(anyString())).thenReturn(null);
+
+        boolean result = userBean.registerUser("test@test.com", "Password1!", "Password2!");
+
+        assertFalse(result);
+    }
+
+}
