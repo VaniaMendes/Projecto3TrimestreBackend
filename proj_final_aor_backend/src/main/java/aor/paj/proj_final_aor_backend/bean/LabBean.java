ec8be8565d96139f266d8dc4b93d0781b35a672f
Cesar Temudo <cesarneves@student.dei.uc.pt>
2024-05-21 10:57:12 +0100

    Create All Labs added

--- proj_final_aor_backend/src/main/java/aor/paj/proj_final_aor_backend/bean/LabBean.java
+++ proj_final_aor_backend/src/main/java/aor/paj/proj_final_aor_backend/bean/LabBean.java
@@ -3,6 +3,7 @@ package aor.paj.proj_final_aor_backend.bean;
 import aor.paj.proj_final_aor_backend.dao.LabDao;
 import aor.paj.proj_final_aor_backend.dto.Lab;
 import aor.paj.proj_final_aor_backend.entity.LabEntity;
+import aor.paj.proj_final_aor_backend.util.enums.Workplace;
 import jakarta.ejb.EJB;
 import jakarta.ejb.Stateless;
 import org.apache.logging.log4j.LogManager;
@@ -24,6 +25,28 @@ public class LabBean implements Serializable {
     public LabBean(LabDao labDao) {
         this.labDao = labDao;
     }
+
+    public void createAllLabs() {
+        for (Workplace workplace : Workplace.values()) {
+            Lab lab = new Lab();
+            lab.setName(workplace);
+            if (labDao.findLabByName(workplace.name()) == null) {
+                createLab(lab);
+            } else {
+                logger.info("Lab already exists: " + workplace.name());
+            }
+        }
+    }
+
+    private LabEntity createLab(Lab lab) {
+        LabEntity labEntity = convertToEntity(lab);
+
+        labDao.persist(labEntity);
+
+        return labEntity;
+    }
+
+
 
     public LabEntity convertToEntity(Lab lab) {
         LabEntity labEntity = new LabEntity();
