b6c18ca591463e45551c4757e58dc2b6ad389baa
Vânia Mendes <vaniamendes@student.dei.uc.pt>
2024-05-22 14:55:57 +0100

    querys tabela user_skill

--- proj_final_aor_backend/src/main/java/aor/paj/proj_final_aor_backend/bean/SkillBean.java
+++ proj_final_aor_backend/src/main/java/aor/paj/proj_final_aor_backend/bean/SkillBean.java
@@ -68,6 +68,4 @@ public class SkillBean implements Serializable {
     }
 
 
-
-
 }
--- proj_final_aor_backend/src/main/java/aor/paj/proj_final_aor_backend/dao/UserSkillDao.java
+++ proj_final_aor_backend/src/main/java/aor/paj/proj_final_aor_backend/dao/UserSkillDao.java
@@ -1,7 +1,11 @@
 package aor.paj.proj_final_aor_backend.dao;
 
+import aor.paj.proj_final_aor_backend.entity.SkillEntity;
+import aor.paj.proj_final_aor_backend.entity.UserEntity;
 import aor.paj.proj_final_aor_backend.entity.UserSkillEntity;
 import jakarta.ejb.Stateless;
+
+import java.util.List;
 
 @Stateless
 public class UserSkillDao extends AbstractDao<UserSkillEntity>{
@@ -10,6 +14,22 @@ public class UserSkillDao extends AbstractDao<UserSkillEntity>{
 
     public UserSkillDao() {
         super(UserSkillEntity.class);
+    }
+
+    public List<UserEntity> findAllUsersWithSkill(long skillId) {
+        try {
+            return em.createNamedQuery("UserSkillEntity.findAllUsersWithSkill").setParameter("skill", skillId).getResultList();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public List<SkillEntity> findAllSkillsForUser(long userId) {
+        try {
+            return em.createNamedQuery("UserSkillEntity.findAllSkillsForUser").setParameter("user", userId).getResultList();
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     public void createUserSkill(UserSkillEntity userSkill) {
