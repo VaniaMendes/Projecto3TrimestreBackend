2d8e5616b93e4da5bd63d2a3f24342d9ac265c59
Vânia Mendes <vaniamendes@student.dei.uc.pt>
2024-07-11 12:05:30 +0100

    email enviado com link https

--- proj_final_aor_backend/src/main/java/aor/paj/proj_final_aor_backend/bean/UserBean.java
+++ proj_final_aor_backend/src/main/java/aor/paj/proj_final_aor_backend/bean/UserBean.java
@@ -401,7 +401,7 @@ public class UserBean implements Serializable {
         body.append("<h1>Welcome to Innovation Lab Management!</h1>");
         body.append("<p>Dear User,</p>");
         body.append("<p>You requested to recover your password. Please click the link below to recover your password:</p>");
-        body.append("<p><a href=\"http://localhost:3000/change-password?token=" + resetPassToken + "\">Recover Password</a></p>");
+        body.append("<p><a href=\"https://localhost:3000/change-password?token=" + resetPassToken + "\">Recover Password</a></p>");
         body.append("<p>If you did not request to recover your password, please ignore this email.</p>");
         body.append("<p>Best regards,<br>Critical Software</p>");
         body.append("</body>");
@@ -471,7 +471,7 @@ public class UserBean implements Serializable {
         body.append("<p>Dear User,</p>");
         body.append("<p>Thank you for registering with us. We're excited to have you on board.</p>");
         body.append("<p>To complete your registration, please click the link below to confirm your account:</p>");
-        body.append("<p><a href=\"http://localhost:3000/confirm-account?token=" + token + "\">Confirm Account</a></p>");
+        body.append("<p><a href=\"https://localhost:3000/confirm-account?token=" + token + "\">Confirm Account</a></p>");
         body.append("<p>If you did not register for our service, please ignore this email.</p>");
         body.append("<p>Best regards,<br>Critical Software</p>");
         body.append("</body>");
